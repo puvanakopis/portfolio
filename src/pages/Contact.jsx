944aeb0393662c7cfe03e2bb827547e8dc9d3939
puvanakopis <puvanakopis@gmail.com>
2025-07-30 13:05:13 +0530

    Add contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import './Contact.css';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="contact" id="contact">
+        {/* ------------ Contact Info ------------ */}
+        <div className='info-container'>
+          <h2 className="info-title">Get In Touch</h2>
 
-export default Contact
+          <div className="contact-info">
+            <div className="info-item">
+              <h3>Email</h3>
+              <p>puvanakopis@gmail.com</p>
+            </div>
+            <div className="info-item">
+              <h3>Phone</h3>
+              <p>+94 75 46 14 044</p>
+            </div>
+            <div className="info-item">
+              <h3>Location</h3>
+              <p>Batticaloa, Sri Lanka</p>
+            </div>
+          </div>
+        </div>
+
+
+        {/* ------------ Contact Form ------------ */}
+        <form className="contact-form">
+          <div className="form-group">
+            <input type="text" placeholder="Your Name" required />
+          </div>
+          <div className="form-group">
+            <input type="email" placeholder="Your Email" required />
+          </div>
+          <div className="form-group">
+            <input type="text" placeholder="Subject" required />
+          </div>
+          <div className="form-group">
+            <textarea placeholder="Your Message" rows="5" required></textarea>
+          </div>
+          <button type="submit" className="btn btn-1 btn-dark">Send Message</button>
+        </form>
+        
+    </div>
+  );
+};
+
+export default Contact;
